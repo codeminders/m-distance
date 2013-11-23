a9ba4c552347eb3b498436850972c2b7199f78c3
Alexander Sova <bird@codeminders.com>
2013-11-22 17:47:12 -0800

    python-fitbit lib added

---
+++ lib/fitbit/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Fitbit API Library
+------------------
+
+:copyright: 2012 ORCAS.
+:license: BSD, see LICENSE for more details.
+"""
+
+from .api import Fitbit, FitbitConsumer, FitbitOauthClient
+
+# Meta.
+
+__title__ = 'fitbit'
+__author__ = 'Issac Kelly and ORCAS'
+__author_email__ = 'pperez@orcasinc.com'
+__copyright__ = 'Copyright 2012 ORCAS'
+__license__ = 'Apache 2.0'
+
+__version__ = '0.0.2'
+
+# Module namespace.
---
+++ lib/fitbit/exceptions.py
@@ -0,0 +1,48 @@
+import json
+
+class BadResponse(Exception):
+    """
+    Currently used if the response can't be json encoded, despite a .json extension
+    """
+    pass
+
+class DeleteError(Exception):
+    """
+    Used when a delete request did not return a 204
+    """
+    pass
+
+class HTTPException(Exception):
+    def __init__(self, response, *args, **kwargs):
+        try:
+            errors = json.loads(response.content)['errors']
+            message = '\n'.join([error['message'] for error in errors])
+        except Exception:
+            message = response
+        super(HTTPException, self).__init__(message, *args, **kwargs)
+
+class HTTPBadRequest(HTTPException):
+    pass
+
+
+class HTTPUnauthorized(HTTPException):
+    pass
+
+
+class HTTPForbidden(HTTPException):
+    pass
+
+
+class HTTPServerError(HTTPException):
+    pass
+
+
+class HTTPConflict(HTTPException):
+    """
+    Used by Fitbit as rate limiter
+    """
+    pass
+
+
+class HTTPNotFound(HTTPException):
+    pass
